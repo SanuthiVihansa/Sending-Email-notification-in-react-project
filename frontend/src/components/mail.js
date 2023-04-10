069d7ee22d2824057b81b0a197c5e7ef13d077b3
SanuthiVihansa <subasofts@gmail.com>
2023-04-10 18:11:02 +0530

    commit mail.js changes

--- frontend/src/components/mail.js
+++ frontend/src/components/mail.js
@@ -1,7 +1,21 @@
-export default function mail(){
+const mail =()=>{
+    const [formState,setFormState]=useState({});
+
+const changeHandler = (event)=>{
+    setFormState({...formState,[event.target.name]: event.target.value});
+}
+
     return(
         <div>
-            <h1>This is the sample </h1>
+            <h1>Sample Email</h1>
+            <form>
+                Name : <input type="text" name = "name" value="" onChange={changeHandler}/> 
+                <br></br>
+                Email :<input type="email" name = "email" value="" onChange={changeHandler}/>
+                <input type="Submit" value="Send Email"/>
+            </form>
         </div>
-    )
-}
+    );
+};
+
+export default mail;
