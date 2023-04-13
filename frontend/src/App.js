9e4ae4fa0b5f0be09fe5aa701d58db2e67b19126
SanuthiVihansa <subasofts@gmail.com>
2023-04-13 23:52:50 +0530

    fix issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, } from "react-router-dom";
-import Mail from './components/Mail';
+import Mail from './components/mail';
 
 
 function App() {
