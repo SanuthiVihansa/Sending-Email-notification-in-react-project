c87ead16f6a5b8475429eb60e6c53349d7b38759
SanuthiVihansa <subasofts@gmail.com>
2023-04-14 00:05:25 +0530

    created a config object

--- frontend/src/components/mail.js
+++ frontend/src/components/mail.js
@@ -3,6 +3,16 @@ import React, { useState } from 'react';
 const Mail = () => {
     const [formState, setFormState] = useState({ name: '', email: '' });
 
+    const config ={
+        Host : "smtp.elasticemail.com",
+        Username : "sanumail@yopmail.com",
+        Password : "8F083563841154A221EBC1B85D41139D6ED4",
+        Port:2525,
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }
     const changeHandler = (event) => {
         setFormState({ ...formState, [event.target.name]: event.target.value });
     };
